fb50b1dfa0ba35e34a8b783bb7bd822058158fa3
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-11-08 08:50:06 +0500

    Add image domains to Next.js configuration for external image support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["randomuser.me", "api.web3forms.com"],
+  }  
 };
 
 export default nextConfig;
